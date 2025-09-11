74d40c67cd38512144123458a5134856333b8d30
ReiO <113203661+Thevi99@users.noreply.github.com>
2025-09-11 09:48:20 +0700

    Fix: UIListLayout

--- MaclibUI.lua
+++ MaclibUI.lua
@@ -1617,13 +1617,13 @@ function MacLib:Window(Settings)
 			tabSwitcher.TextColor3 = Color3.fromRGB(0, 0, 0)
 			tabSwitcher.TextSize = 14
 			tabSwitcher.AutoButtonColor = false
-			tabSwitcher.AnchorPoint = Vector2.new(0, 0)
+			tabSwitcher.AnchorPoint = Vector2.new(0.5, 0)
 			tabSwitcher.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 			tabSwitcher.BackgroundTransparency = 1
 			tabSwitcher.BorderColor3 = Color3.fromRGB(0, 0, 0)
 			tabSwitcher.BorderSizePixel = 0
-			tabSwitcher.Position = UDim2.fromScale(0, 0)
-			tabSwitcher.Size = UDim2.new(1, -21, 0, 40)
+			tabSwitcher.Position = UDim2.fromScale(0.5, 0)
+			tabSwitcher.Size = UDim2.new(0, 200, 0, 40)
 
 			tabIndex += 1
 			tabSwitcher.LayoutOrder = tabIndex
